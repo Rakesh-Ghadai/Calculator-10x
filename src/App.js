2fe1d7d8ff073bf446a72ab0a81b98d1dc98de0a
kumar2rakesh <kumarrakeshghadai@gmail.com>
2022-08-08 21:32:06 +0530

    calculator app deployed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [res,setRes]=useState("");
+  const handleClick = (e)=>{
+    setRes(res.concat(e.target.value));
+  }
+
+  const handleClear = ()=>{
+    setRes("");
+  }
+
+  const handleResult = ()=>{
+    setRes(eval(res).toString());
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='main_div'>
+      <div className='row title'>Calculator</div>
+      <div className='row'>
+      <input type='text' value={res} className='text'/>
+      <input type='button' value='C' onClick={handleClear} className='button_one'/>
+      </div>
+
+      <div className='row'>
+      <input type='button' value='1' onClick={handleClick} className='button'/>
+      <input type='button' value='2' onClick={handleClick} className='button'/>
+      <input type='button' value='3' onClick={handleClick} className='button'/>
+      <input type='button' value='/' onClick={handleClick} className='button_one'/>
+      </div>
+
+      <div className='row'>
+      <input type='button' value='4' onClick={handleClick} className='button'/>
+      <input type='button' value='5' onClick={handleClick} className='button'/>
+      <input type='button' value='6' onClick={handleClick} className='button'/>
+      <input type='button' value='-' onClick={handleClick} className='button_one'/>
+      </div>
+
+      <div className='row'>
+      <input type='button' value='7' onClick={handleClick} className='button'/>
+      <input type='button' value='8' onClick={handleClick} className='button'/>
+      <input type='button' value='9' onClick={handleClick} className='button'/>
+      <input type='button' value='+' onClick={handleClick} className='button_one'/> 
+      </div>
+
+      <div className='row'>
+      <input type='button' value='.' onClick={handleClick} className='button'/>
+      <input type='button' value='0' onClick={handleClick} className='button'/>
+      <input type='button' value='=' onClick={handleResult} className='button'/>
+      <input type='button' value='*' onClick={handleClick} className='button_one'/>
+      </div>
+  
     </div>
+    
+    </>
+ 
   );
 }
 
